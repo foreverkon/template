eb7dcf998171da06c56e47b19754dae971d20559
konforever <ycz1776537118@gmail.com>
2022-05-07 11:59:42 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# A command-line tool to download some commonly used template files (for myself).
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "template/cmd"
+
+func main() {
+	cmd.Execute()
+}
